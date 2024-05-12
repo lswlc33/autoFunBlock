3db4dcb7507e7ca26a9b4c2f8749ea1582c651a1
lswlc33 <lswlc33@outlook.com>
2024-05-12 23:28:37 +0800

    添加胜率监控

--- 大逃杀-监控.py
+++ 大逃杀-监控.py
@@ -3,11 +3,13 @@ import os
 import time
 from lib.大逃杀 import 大逃杀_信息, get_real_room
 from lib.雪の函数 import cleanT, pTitle, 当前时间
+from lib.大逃杀计算 import get_win_stat, get_m_stat
 
 
 if __name__ == "__main__":
     data = ""
     当前期数 = 0
+    num = 2000
     if not os.path.exists("data/escape.csv"):
         with open("data/escape.csv", "w", newline="", encoding="utf-8") as f:
             writer = csv.writer(f)
@@ -18,6 +20,8 @@ if __name__ == "__main__":
     while True:
         try:
             data = 大逃杀_信息()
+            win, lose, win_rate = get_win_stat(num)
+            paid_m, win_m, total_m = get_m_stat(num)
             cleanT()
             print(
                 f"\n {pTitle('大逃杀监控')}\n\n",
@@ -27,8 +31,10 @@ if __name__ == "__main__":
                 f"倒计时: {data['countdown']}\t\t是否获胜: {data['myIsWin']}\n",
                 f"本期击杀: {get_real_room(data['killNumber'])}\t上期击杀: {get_real_room(data['prevRoomNumber'])}\n",
                 f"是否结算: {'是' if data['state']==2 else '否'}\t\t我的宝石: {data['myWallet']}\n",
+                f"\n {pTitle(f'近 {num} 场数据胜率')}\n\n",
+                f"胜场: {win}\t败场: {lose}\t胜率: {win_rate}\n",
+                f"投入: {paid_m}\t赚的: {win_m}\t利润: {total_m}",
             )
-
             if data["state"] == 2 and 当前期数 != data["issue"]:
                 with open("data/escape.csv", "a", newline="", encoding="utf-8") as f:
                     writer = csv.writer(f)
