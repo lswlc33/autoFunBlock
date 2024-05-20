6be82aefe0a12ecd2a381d0aebe970c0a3bafa3f
lswlc33 <lswlc33@outlook.com>
2024-05-20 12:33:55 +0800

    完善定投脚本

--- 大逃杀-定投.py
+++ 大逃杀-定投.py
@@ -1,15 +1,14 @@
 import time, os
 from lib.大逃杀 import get_real_room, 大逃杀_信息, 大逃杀_投入
-from lib.雪の函数 import 当前时间
-from lib.大逃杀计算 import get_best_room
+from lib.雪の函数 import 当前时间, cleanT
 
-os.system("clear")
+cleanT()
 
 is_paid = 0
 is_upated = 0
-投入宝石 = 0.1
+投入宝石 = input("输入投入定投宝石: ")
 issue = 大逃杀_信息()["issue"]
-roomid = 0
+roomid = input("输入房间号(1-8): ")
 
 while True:
     try:
@@ -19,7 +18,7 @@ while True:
             if is_paid:
                 red = "\033[31m凉了\033[39m"
                 print(
-                    f"{当前时间()}  第 {new_issue} 期 {red if roomid==int(prevRoomNumber) else '赢了'}"
+                    f"{当前时间()}  第 {new_issue-1} 期 {red if roomid==int(prevRoomNumber) else '赢了'}"
                 )
             print(f"{当前时间()}  进入第 {new_issue} 期")
             issue = new_issue
@@ -27,7 +26,6 @@ while True:
             is_upated = 1
 
         if 5 < 大逃杀_信息()["countdown"] < 40 and not is_paid:
-            roomid = get_best_room(2)
             大逃杀_投入(roomNumber=roomid, costMedal=投入宝石)
             print(f"{当前时间()}  投入 {get_real_room(roomid)} 宝石 {投入宝石}")
 
