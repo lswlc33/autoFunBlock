949dc18083fb1f72142de0e9b2d637ea21260dae
lswlc33 <lswlc33@outlook.com>
2024-05-23 12:31:54 +0800

    添加一个脚本

--- lib/贝壳.py
+++ lib/贝壳.py
@@ -1,6 +1,7 @@
 import requests
 from lib.登录信息 import headers
 
+
 session = requests.Session()
 session.headers = headers
 
@@ -42,3 +43,32 @@ def 贝壳交易(type, tradeId, quantity):
         url=urls[type], data=f"tradeId={tradeId}&quantity={quantity}"
     )
     return response.json()
+
+
+def get_shells_trade_log(page=1):
+    return requests.post(
+        url="https://block-api.lucklyworld.com/v6/api/pets/shells/trade/logs",
+        data=f"page={page}",
+        headers=headers,
+    ).json()
+
+
+def get_shells_trade_logs():
+    """获取所有页并写入文件"""
+    hasMore = True
+    next = 1
+    trade_list = []
+
+    while hasMore:
+        print(f"\r正在获取第 {next} 页 当前 {len(trade_list)} 条记录", end="")
+        data = get_shells_trade_log(next)
+        hasMore = data["hasMore"]
+        next = data["next"]
+        trade_list += data["list"]
+
+    print(f"\r获取完成, 共 {next} 页 {len(trade_list)} 条记录,正在写入", end="")
+
+    with open("data/shells_trade_logs.txt", "w") as f:
+        f.write(str(trade_list))
+
+    print(f"\r写入完成 data/shells_trade_logs.txt", end="")
