d74b0a0fbfe452595fb77cf8640077bf0cc16e09
lswlc33 <lswlc33@outlook.com>
2024-05-24 20:10:30 +0800

    1

--- 贝壳-倒狗.py
+++ 贝壳-倒狗.py
@@ -15,9 +15,10 @@ shells_info = {
     "tradeId2": None,
     "quantity2": None,
     "price2": None,
-    "is_sold" : True,
-    "sold_quantity" : 0
+    "is_sold": True,
+    "sold_quantity": 0,
 }
+
 
 def update_wallet_info():
     """钱包"""
@@ -31,6 +32,7 @@ def update_wallet_info():
     wallet_info_timer = threading.Timer(1, update_wallet_info)
     wallet_info_timer.start()
 
+
 def update_market_info():
     """更新市场信息"""
     global shells_info
@@ -38,15 +40,21 @@ def update_market_info():
     data1 = 贝壳市场(0)[0]
     data2 = 贝壳市场(1)[0]
 
-    if shells_info["tradeId1"] != int(data1["tradeId"]) or shells_info["tradeId2"] != int(data2["tradeId"]):
+    if shells_info["tradeId1"] != int(data1["tradeId"]) or shells_info[
+        "tradeId2"
+    ] != int(data2["tradeId"]):
         shells_info["price1"] = float(data1["price"])
         shells_info["price2"] = float(data2["price"])
         shells_info["quantity1"] = int(data1["quantity"])
         shells_info["quantity2"] = int(data2["quantity"])
         print(
-            shells_info["myrocks"],shells_info["myshells"] ,
-            shells_info["price1"], shells_info["price2"],
-            shells_info["quantity1"], shells_info["quantity2"])
+            shells_info["myrocks"],
+            shells_info["myshells"],
+            shells_info["price1"],
+            shells_info["price2"],
+            shells_info["quantity1"],
+            shells_info["quantity2"],
+        )
 
     shells_info["tradeId1"] = int(data1["tradeId"])
     shells_info["tradeId2"] = int(data2["tradeId"])
@@ -56,17 +64,18 @@ def buy_shell(id, quantity):
     """购买"""
     r = 贝壳交易(0, id, quantity)
     if r.get("errorCode"):
-        print(f"购买失败: {r.get("message")}")
+        print(f"购买失败: {r.get('message')}")
         return False
     else:
         print("购买成功")
         return True
 
+
 def sell_shell(id, quantity):
     """出售"""
     r = 贝壳交易(1, id, quantity)
     if r.get("errorCode"):
-        print(f"出售失败: {r.get("message")}")
+        print(f"出售失败: {r.get('message')}")
         return False
     else:
         print("出售成功")
@@ -79,16 +88,18 @@ def check_market_loop():
     if not shells_info["is_sold"]:
         print("补救流程")
         print(shells_info["is_sold"])
-        shells_info["is_sold"] = sell_shell(shells_info["tradeId1"], shells_info["sold_quantity"])
+        shells_info["is_sold"] = sell_shell(
+            shells_info["tradeId1"], shells_info["sold_quantity"]
+        )
 
     elif shells_info["myshells"] >= 300.00:
         print("闲置流程")
-        quantity = min(200,shells_info["myshells"] - 201)
+        quantity = min(200, shells_info["myshells"] - 201)
         sell_shell(shells_info["tradeId1"], quantity - 10)
 
     elif shells_info["price1"] >= 0.01:
         print("高价流程")
-        quantity = min(shells_info["quantity1"],shells_info["myshells"] - 201)
+        quantity = min(shells_info["quantity1"], shells_info["myshells"] - 201)
         sell_shell(shells_info["tradeId1"], quantity)
 
     elif shells_info["price2"] < 0.001:
@@ -102,6 +113,7 @@ def check_market_loop():
             shells_info["is_sold"] = sell_shell(shells_info["tradeId1"], quantity)
             shells_info["sold_quantity"] = quantity
         time.sleep(1)
+
 
 print("\033c\033[?25l", end="")
 
